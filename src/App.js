ba387bbad299369c3937e5b4290d7c9c0e65a0d1
Omar Cossio <omar119mce@gmail.com>
2021-02-05 22:05:18 -0500

    re-push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Break from "./components/Break";
+import Sessions from "./components/Sessions";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Break />
+      <div className="container">
+       <Header />
+        <div className="row">
+          <div className="col-4">
+            <Break />
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-4">
+            <Sessions />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -21,12 +21,14 @@ const Break = () => {
 
     const timeToMins = moment.duration(breakLength, 's').minutes();
 
-    return(
-        <div>
-            <p id="break-label">Break</p>
-            <p id="break-length">{timeToMins}</p>
-            <button id="break-decrement" onClick={decrementByOneMin}>-</button>
-            <button id="break-increment" onClick={incrementByOneMin}>+</button>
+    return (
+        <div className="container text-center">
+            <div className="card">
+                <p id="break-label">Break</p>
+                <p id="break-length">{timeToMins}</p>
+                <button id="break-decrement" onClick={decrementByOneMin}>-</button>
+                <button id="break-increment" onClick={incrementByOneMin}>+</button>
+            </div>
         </div>
     );
 }
