70e7385d42e2a45de8975d0927ef09b5f725e07c
Omar Cossio <omar119mce@gmail.com>
2021-02-05 22:16:06 -0500

    fixed bootstrap bugs

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <container className="container">
+        <header className="Header text-center"><h1>Pomodoro Clock</h1></header>
+        <hr></hr>
+        </container>
+    );
+}
+
+export default Header;
--- src/components/Sessions.jsx
+++ src/components/Sessions.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import moment from "moment";
 
 const Session = () => {
-    const [sessionLength, setSessionLength] = useState(300)
+    const [sessionLength, setSessionLength] = useState(60 * 25)
 
     const decrementByOneMin = () => {
         const newSessionLength = sessionLength - 60;
@@ -21,11 +21,13 @@ const Session = () => {
     const timeToMins = moment.duration(sessionLength, 's').minutes();
 
     return(
-        <div>
+        <div className="container text-center">
+            <div className="card">
             <p id="session-label">Session</p>
             <p id="session-length">{timeToMins}</p>
             <button id="session-decrement" onClick={decrementByOneMin}>-</button>
             <button id="session-increment" onClick={incrementByOneMin}>+</button>
+            </div>
         </div>
     );
 }
