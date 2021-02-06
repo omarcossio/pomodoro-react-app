b68bc1c3b9d81e555c2e8ee08eabd25070584861
Omar Cossio <omar119mce@gmail.com>
2021-02-05 21:01:16 -0500

    time from seconds to mins

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
+import moment from "moment";
 
 
 const Break = () => {
     const [breakLength, setBreakLength] = useState(300)
 
+    const decrementByOneMin = () => {
+        const newBreakLength = breakLength - 60;
+
+        if (newBreakLength < 0) {
+            setBreakLength(0);
+        } else {
+            setBreakLength(newBreakLength);
+        }
+    };
+
+    const incrementByOneMin = () => {
+        setBreakLength(breakLength + 60);
+    }
+
+    const timeToMins = moment.duration(breakLength, 's').minutes();
 
     return(
         <div>
             <p id="break-label">Break</p>
-            <p id="break-length">{breakLength}</p>
+            <p id="break-length">{timeToMins}</p>
+            <button id="break-decrement" onClick={decrementByOneMin}>-</button>
+            <button id="break-increment" onClick={incrementByOneMin}>+</button>
         </div>
     );
 }
